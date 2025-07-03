8ee769f515d8302b1f7ec0b5d9d4b9704fc1a1ca
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2025-07-04 03:00:40 +0500

    в

--- gamee/lib/main.dart
+++ gamee/lib/main.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  FlutterError.onError = (FlutterErrorDetails details) {
-    FlutterError.presentError(details);
-    Zone.current.handleUncaughtError(details.exception, details.stack!);
-  };
   runZonedGuarded<Future<void>>(
     () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      FlutterError.onError = (FlutterErrorDetails details) {
+        FlutterError.presentError(details);
+        Zone.current.handleUncaughtError(details.exception, details.stack!);
+      };
+
       runApp(const GameeApp());
     },
     (error, stack) {
