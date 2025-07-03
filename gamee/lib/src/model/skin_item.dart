8cf4306870d8e6367cca8491085c4586afd03450
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2025-07-03 14:53:35 +0500

    Add store page with purchasing logic

---
+++ gamee/lib/src/model/skin_item.dart
@@ -0,0 +1,13 @@
+class SkinItem {
+  final int id;
+  final String title;
+  final String image;
+  final int price;
+
+  const SkinItem({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+}
