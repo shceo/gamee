2d3e2a5b15aebc30d76f6fa63cd8f5756eb05872
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2025-07-03 11:43:19 +0500

    Add shooting and enemy health

--- gamee/lib/src/view_model/game_state.dart
+++ gamee/lib/src/view_model/game_state.dart
@@ -1,11 +1,13 @@
 import '../model/obstacle.dart';
 import '../model/game_mode.dart';
+import '../model/bullet.dart';
 
 class GameState {
   final bool isRunning;
   final bool isGameOver;
   final double playerX;
   final List<Obstacle> obstacles;
+  final List<Bullet> bullets;
   final int coins;
   final int level;
   final GameMode mode;
@@ -15,6 +17,7 @@ class GameState {
     this.isGameOver = false,
     this.playerX = 0.5,
     this.obstacles = const [],
+    this.bullets = const [],
     this.coins = 0,
     this.level = 1,
     this.mode = GameMode.arcade,
@@ -25,6 +28,7 @@ class GameState {
     bool? isGameOver,
     double? playerX,
     List<Obstacle>? obstacles,
+    List<Bullet>? bullets,
     int? coins,
     int? level,
     GameMode? mode,
@@ -34,6 +38,7 @@ class GameState {
       isGameOver: isGameOver ?? this.isGameOver,
       playerX: playerX ?? this.playerX,
       obstacles: obstacles ?? this.obstacles,
+      bullets: bullets ?? this.bullets,
       coins: coins ?? this.coins,
       level: level ?? this.level,
       mode: mode ?? this.mode,
